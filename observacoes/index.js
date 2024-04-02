5f3a03015761f4c1429f46fa8c70a6ac18571167
ArnaldWeslen05 <arnaldclash@gmail.com>
2024-04-01 23:08:29 -0300

    correção

--- observacoes/index.js
+++ observacoes/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const {v4: uuidv4} =require('uuid')
 const app = express();
 const observacoesPorlembreteId = {}
+
 
 app.use(bodyParse.json());
 
 app.post('/lembretes/:id/observacoes', (req,res) => {
-
+    const idDbs = uuidv4();
+    const {texto } = req.body;
 })
 
 app.get('/lembretes/:id/observacoes', (req,res) => {
