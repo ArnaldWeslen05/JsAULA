430de3d3324e4ac2406eec584fb625683d28735d
ArnaldWeslen05 <arnaldclash@gmail.com>
2024-04-08 20:47:05 -0300

    2.0

--- observacoes/index.js
+++ observacoes/index.js
@@ -9,10 +9,17 @@ app.use(bodyParse.json());
 app.post('/lembretes/:id/observacoes', (req,res) => {
     const idDbs = uuidv4();
     const {texto } = req.body;
-})
+    const observacoesDoLembrete =
+    observacoesPorLembreteId[req.params.id] || [];
+    observacoesDoLembrete.push({id: idObs, texto});
+    observacoesPorlembreteId[req.params.id]= observacoesDoLembrete;
+    res.status(201).send(observacoesDoLembrete);
+    
+});
 
 app.get('/lembretes/:id/observacoes', (req,res) => {
-
+   res.send(observacoesPorlembreteId[req.params
+.id] || [])
 })
 
 app.listen(5000, () => {
